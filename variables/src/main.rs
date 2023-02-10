74adc5e48d2f24547e83ae068db7030fe237a03c
Daniel Victoriano <djose1164@gmail.com>
2023-02-10 11:26:10 -0400

    shadowing with number

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let x = 5;
 
-    // Constants
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The values of x in the inner scope is: {x}");    
+    }
+
+    println!("The values of x is: {x}");
 }
