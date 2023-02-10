25eed21e4cdba74ab081a27e58db37385401b1a4
Daniel Victoriano <djose1164@gmail.com>
2023-02-10 11:31:13 -0400

    shadowing with different types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The values of x in the inner scope is: {x}");    
-    }
-
-    println!("The values of x is: {x}");
+    let mut spaces = "    ";
+     spaces = spaces.len();
 }
