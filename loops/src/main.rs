6e0fcc4e5395789413001062cd45ff1c4eb1b367
Daniel Victoriano <djose1164@gmail.com>
2023-02-11 15:56:58 -0400

    while loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,20 +1,11 @@
 fn main() {
-    let mut counter = 0;
-    'counting_up: loop {
-        println!("counter = {counter}");
-        let mut remaining = 10;
+    let mut number = 3;
+ 
+    while number != 0 {
+        println!("{number}");
 
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 { break; }
-
-            if counter == 2 { break 'counting_up; }
-
-            remaining -= 1;
-        }
-
-        counter += 1;
+        number -= 1;
     }
 
-    println!("End counter = {counter}");
+    println!("Lift off!");
 }
