19725db33446b593a4af3f54de3c69859f61b428
Daniel Victoriano <djose1164@gmail.com>
2023-02-11 15:54:50 -0400

    labeled loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
     let mut counter = 0;
+    'counting_up: loop {
+        println!("counter = {counter}");
+        let mut remaining = 10;
 
-    let result = loop {
-        counter += 1;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 { break; }
 
-        if counter == 10 {
-            break counter * 2;
+            if counter == 2 { break 'counting_up; }
+
+            remaining -= 1;
         }
-    };
 
-    println!("The result is {result}");
+        counter += 1;
+    }
+
+    println!("End counter = {counter}");
 }
