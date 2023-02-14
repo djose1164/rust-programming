cac8bbf5f3142279cf5afb5b07cbec17eaf2e15f
Daniel Victoriano <djose1164@gmail.com>
2023-02-14 16:39:11 -0400

    Fibo generator.

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    println!("***The Fibo generator***");
+    println!("Enter a number (be careful, this can take some time): ");
+
+    let mut number = String::new();
+    std::io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line.");
+
+    let number: i32 = number
+        .trim()
+        .parse()
+        .expect("It doesn't look like a number.");
+
+    match number {
+        0 => println!("0"),
+        1 => println!("1"),
+        _ => {
+            let mut counter = 0;
+            let mut previous = 0;
+            let mut current = 1;
+            print!("0, 1, ");
+            while counter != number {
+                let tmp = current;
+                current = previous + current;
+                previous = tmp;
+                if counter == number - 1 {
+                    print!("{current}.");
+                } else {
+                    print!("{current}, ")
+                }
+                counter += 1;
+            }
+        }
+    }
+}
