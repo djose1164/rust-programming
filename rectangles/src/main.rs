cba0eefbc8d727a79ddce3cd7ef220d039a24819
Daniel Victoriano <djose1164@gmail.com>
2023-02-16 14:00:24 -0400

    Refactoring with Structs: Adding More Meaning

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,20 @@
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
 
 fn main() {
-    let rec = (30, 10);
+    let rec = Rectangle {
+        width: 30,
+        height: 10
+    };
 
     println!(
         "The area of the rectangles is {} square pixels.",
-        area(rec)
+        area(&rec)
     );
 }
