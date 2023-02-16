fea0379852e7586e495a996f65de475945a51fc0
Daniel Victoriano <djose1164@gmail.com>
2023-02-16 11:45:37 -0400

    Slices

---
+++ slices/src/main.rs
@@ -0,0 +1,16 @@
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i]
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let name = String::from("Jose Daniel");
+    println!("first word: {}", first_word(&name));
+}
